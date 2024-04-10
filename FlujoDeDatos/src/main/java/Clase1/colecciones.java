d95caa7e9892ec7727914606ccc6d6aab070f0b8
TomAlF16 <101424944+TomAlF16@users.noreply.github.com>
2024-04-10 12:37:35 -0300

    a

---
+++ FlujoDeDatos/src/main/java/Clase1/colecciones.java
@@ -0,0 +1,20 @@
+package Clase1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class colecciones {
+	int[] vec = new int[5];
+	
+	ArrayList<String> frutas = new ArrayList<>();
+	ArrayList verduras = new ArrayList<>();
+	
+	List nombres = new ArrayList<>();
+	List<String> apellido = new ArrayList<>();
+
+	LinkedList<String> componentesPc = new LinkedList<>();
+    List Precios = new LinkedList<>();
+    
+    Map<> españolIngles = new colecciones();
+}
--- FlujoDeDatos/src/main/java/Clase1/main.java
+++ FlujoDeDatos/src/main/java/Clase1/main.java
@@ -5,17 +5,12 @@ import java.io.PrintStream;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Lectura leer = new Lectura();
+		Lectura leer = new Lectura();
+		JavaString repaso = new JavaString();
+		//repaso.respaso();
 		
-		//String leido = leer.entradaDeDatos();
-		PrintStream ps = new PrintStream( System.out );
-		String texto = "    Este va a ser el, texto <> que yo quiero , manipular.";
-		
-		ps.println("El char 3:" +  texto.charAt(3) );
-		ps.println("sin espacio:" + texto.trim()  );
-		ps.println("'el' esta en la posicion':" + texto.indexOf("el"));
-		ps.println("el '>' este ne la pocicion:" + texto.indexOf( '>' ) );
+		ClassReader lectura = new ClassReader();
+		System.out.println(lectura.leer() );
+	     
 	}
-
 }
