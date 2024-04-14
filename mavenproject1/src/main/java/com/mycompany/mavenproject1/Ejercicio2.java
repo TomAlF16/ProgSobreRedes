dbac3b37bcf0b2e197f6bd06c529973b4e006359
TomAlF16 <101424944+TomAlF16@users.noreply.github.com>
2024-04-14 18:28:31 -0300

    me quiero matar
    matenme hopy

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Ejercicio2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author tafav
+ */
+public class Ejercicio2 {
+//    Dados los valores de dos de los 
+//    ángulos interiores de un triángulo, la computadora muestra el valor del restante.
+
+    
+//    
+//    float angulo1= 2;
+//    float angulo2= 2;
+//    float angulo3= 180-(angulo1 + angulo2);
+//     public float obtenerResultado() {
+//        return angulo3;
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Guia.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+
+import java.io.PrintStream;
+
+/**
+ *
+ * @author tafav
+ */
+public class Guia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PrintStream ps = new PrintStream(System.out);
+//        ps.println("Hola, mundo!");
+//        ps.print(42);
+//        ps.printf("El valor de PI es %.2f", Math.PI);
+     Ejercicio1 ejercicio1 = new Ejercicio1();
+
+     
+//     System.out.println(Ejercicio1.calcularSueldoBruto(0, 0));
+    }
+
+}
