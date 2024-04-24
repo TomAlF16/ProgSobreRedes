60f2a05d01e9a664ed391025538ec2f9523b2ff3
TomAlF16 <101424944+TomAlF16@users.noreply.github.com>
2024-04-24 13:35:21 -0300

    ujy
    jh

--- FlujoDeDatos/src/main/java/Clase1/colecciones.java
+++ FlujoDeDatos/src/main/java/Clase1/colecciones.java
@@ -1,20 +1,39 @@
 package Clase1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class colecciones {
-	int[] vec = new int[5];
-	
-	ArrayList<String> frutas = new ArrayList<>();
-	ArrayList verduras = new ArrayList<>();
-	
-	List nombres = new ArrayList<>();
-	List<String> apellido = new ArrayList<>();
+	//vector
+		int[] vec = new int[5];
+		
+		ArrayList<String> frutas = new ArrayList<>();
+		ArrayList verduras = new ArrayList<>();
+		
 
-	LinkedList<String> componentesPc = new LinkedList<>();
-    List Precios = new LinkedList<>();
-    
-    Map<> españolIngles = new colecciones();
+		List nombres  = new ArrayList<>();
+		List<String> apellido = new ArrayList<>();
+		
+		LinkedList<String> componentesPc = new LinkedList<>();
+		List precios = new LinkedList<>();
+		
+		LinkedHashMap<String, String> listaEspañolIngles = new LinkedHashMap<>();
+		Map<Integer,Integer> valores = new LinkedHashMap<>();
+		LinkedHashSet<Integer> listaInglesEspañol = new LinkedHashSet<>();
+		Set<Integer> valores2 = new LinkedHashSet<>();	
+		
+		HashMap<String , String> españolRAE = new HashMap<>();
+		Map<String,Integer> españolIngles = new HashMap<>();
+		
+		HashSet<String> claves = new HashSet<>();
+		
+		Set<Integer> DNI = new HashSet<>();
+
 }
